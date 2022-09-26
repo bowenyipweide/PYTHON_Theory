cc7e431e5360a656b9049f0469a1753aae6fd06e
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2022-09-26 14:55:44 +0800

    Create Python_Bowen's.py

---
+++ Python_Bowen's.py
@@ -0,0 +1,27 @@
+#BUG: {An error in a program that prevents the program from running as expected.}
+#Function: {A piece of code that you can easily call over and over again.}
+#Loop: {The action of doing something over and over again.}
+
+
+#Data Types: 
+1) PEMDAS
+2) SYMBOLS
+
+#Rules PEMDAS priorities
+(P) Parenthesis - (a)
+(E) Exponential - (a**b)
+(M) Multiplication - (a*b)
+(D) Division - (a/b)
+(A) Additional - (a+b)
+(S) Subtraction - (a-b) 
+
+Modulus - (a%b)
+
+#SYMBOLS
+Equal to - (a==b)
+Not equal to - (a!=b) or (a<>b)
+Greater than - (a>b)
+Greater than or equal to  - (a>=b)
+Lesser than - (a<b)
+Lesser than or equal to - (a<=b)
+Assigning - (a=b)
