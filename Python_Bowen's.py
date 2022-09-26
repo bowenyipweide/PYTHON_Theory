7d419db141c2ea6e733d2cd53f97e7a4c9eccfc4
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2022-09-26 15:15:19 +0800

    Update Python_Bowen's.py

--- Python_Bowen's.py
+++ Python_Bowen's.py
@@ -6,8 +6,9 @@
 #Data Types: 
 1) PEMDAS
 2) SYMBOLS
+3) Input Functions
 
-#Rules PEMDAS priorities
+1) #Rules PEMDAS priorities
 (P) Parenthesis - (a)
 (E) Exponential - (a**b)
 (M) Multiplication - (a*b)
@@ -17,7 +18,7 @@
 
 Modulus - (a%b)
 
-#SYMBOLS
+2) #SYMBOLS
 Equal to - (a==b)
 Not equal to - (a!=b) or (a<>b)
 Greater than - (a>b)
@@ -25,3 +26,26 @@ Greater than or equal to  - (a>=b)
 Lesser than - (a<b)
 Lesser than or equal to - (a<=b)
 Assigning - (a=b)
+
+3) #Input Functions
+String - ("Hello World")
+
+Concatenate - ("Hello" + "World")
+
+Subscript - print("Hello"[0])
+  output: ("H") #Count at position 0 first, not position 1
+    
+Interger - (123 + 456)
+  output: (123456) 
+    
+Float - ("3.14159") #Decimal is a float
+
+Str - num_char = len(input("What is your name?"))
+#Input bowen
+      new_num_char = str(num_char)
+#5
+      print("Your name has" + new_num_char + "characters")
+  output: (Your name has 5 characters)
+
+Doc String - """ (insert) """ #Comments 
+
