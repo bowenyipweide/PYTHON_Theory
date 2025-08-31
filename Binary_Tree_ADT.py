cd197c33fbb21cf4c670d202672c476721c9d177
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2025-08-31 18:54:40 +0800

    Update Binary_Tree_ADT.py

--- Binary_Tree_ADT.py
+++ Binary_Tree_ADT.py
@@ -1,1 +1,28 @@
+#Binary tree operations INSERT NODE > DELETE NODE > SEARCH NODE > DEPTH / BREATH FIRST TRAVERSAL (IN / PRE / POST ORDER / LEVEL-BY-LEVEL)
 
+#Initiate class node for left & right tree 
+class Node: 
+	def __init__(self, data):
+		self.data = data 
+		self.left = None
+		self.right = None
+#PRE-Order Depth first traversal
+def pre_order_traversal(self, node):
+	if node: 
+		print(node.data, end = "")
+		self.pre_order_traversal(node.left)
+		self.pre_order_traversal(node.right)
+#IN-Order Depth first traversal
+def pre_order_traversal(self, node):
+	if node: 
+		self.pre_order_traversal(node.left)
+		print(node.data, end = "")
+		self.pre_order_traversal(node.right)
+#POST-Order Depth first traversal
+def pre_order_traversal(self, node):
+	if node: 
+		self.pre_order_traversal(node.left)
+		self.pre_order_traversal(node.right)
+		print(node.data, end = "")
+	
+	
