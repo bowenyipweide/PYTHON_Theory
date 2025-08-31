e3bfd41f8b778fd16af5a7c2bab624289acc235f
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2025-08-31 19:02:20 +0800

    Update Binary_Tree_ADT.py

--- Binary_Tree_ADT.py
+++ Binary_Tree_ADT.py
@@ -7,35 +7,35 @@ class Node:
 		self.left = None
 		self.right = None
 #PRE-Order Depth first traversal
-def pre_order_traversal(self, node):
-	if node: 
-		print(node.data, end = "")
-		self.pre_order_traversal(node.left)
-		self.pre_order_traversal(node.right)
+	def pre_order_traversal(self, node):
+		if node: 
+			print(node.data, end = "")
+			self.pre_order_traversal(node.left)
+			self.pre_order_traversal(node.right)
 #IN-Order Depth first traversal
-def pre_order_traversal(self, node):
-	if node: 
-		self.pre_order_traversal(node.left)
-		print(node.data, end = "")
-		self.pre_order_traversal(node.right)
+	def pre_order_traversal(self, node):
+		if node: 
+			self.pre_order_traversal(node.left)
+			print(node.data, end = "")
+			self.pre_order_traversal(node.right)
 #POST-Order Depth first traversal
-def pre_order_traversal(self, node):
-	if node: 
-		self.pre_order_traversal(node.left)
-		self.pre_order_traversal(node.right)
-		print(node.data, end = "")
+	def pre_order_traversal(self, node):
+		if node: 
+			self.pre_order_traversal(node.left)
+			self.pre_order_traversal(node.right)
+			print(node.data, end = "")
 #Level-by-level Depth first traversal
 #(1)Enqueue current node > (2) Dequeue node > (3) Enqueue children > (4) Repeat step (2) until the queue is empty
-def level_order_traversal(self):
-	if not self.root:
-		return
-	queue = deque ([self.root])
-	while queue:
-		current_node = queue.popleft()
-		print(current_node.data, end = "")
-		if current_node.left:
-			queue.append(current_node.left)
-		if curren_node.right:
-			queue.append(current_node.right)
+	def level_order_traversal(self):
+		if not self.root:
+			return
+		queue = deque ([self.root])
+		while queue:
+			current_node = queue.popleft()
+			print(current_node.data, end = "")
+			if current_node.left:
+				queue.append(current_node.left)
+			if curren_node.right:
+				queue.append(current_node.right)
 	
 	
