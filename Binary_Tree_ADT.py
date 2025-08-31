7a621af74308fa802bd65dba83ac9453e0def73b
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2025-08-31 19:01:52 +0800

    Update Binary_Tree_ADT.py

--- Binary_Tree_ADT.py
+++ Binary_Tree_ADT.py
@@ -24,5 +24,18 @@ def pre_order_traversal(self, node):
 		self.pre_order_traversal(node.left)
 		self.pre_order_traversal(node.right)
 		print(node.data, end = "")
+#Level-by-level Depth first traversal
+#(1)Enqueue current node > (2) Dequeue node > (3) Enqueue children > (4) Repeat step (2) until the queue is empty
+def level_order_traversal(self):
+	if not self.root:
+		return
+	queue = deque ([self.root])
+	while queue:
+		current_node = queue.popleft()
+		print(current_node.data, end = "")
+		if current_node.left:
+			queue.append(current_node.left)
+		if curren_node.right:
+			queue.append(current_node.right)
 	
 	
