8b5385ec9f1facb054a308268b1fe5c303663b4a
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2025-09-14 21:42:13 +0800

    Update and rename Searching: Hash Table.py to Searching: Hash_Table.py

---
+++ Searching: Hash_Table.py
@@ -0,0 +1,12 @@
+To design a hash table for data retrieval, we need to consider
+• Hash function
+• Collision and its resolutions
+• Delete a key from a hash table
+• Resizing hash table – Dynamic hash table
+Hashing
+"""_hash_function(key): A private method that hashes the key and determines
+the index in the table.
+• insert(key, value): Inserts a key-value pair into the table. If the key already
+exists, it updates the value.
+• get(key): Retrieves the value associated with a given key.
+• delete(key): Deletes the key-value pair if the key exists.""" 
