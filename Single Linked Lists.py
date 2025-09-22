ee7d04e556ee5060ac3f3fea7c59b89dc96f836e
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2025-09-22 17:17:55 +0800

    Update Single Linked Lists.py

--- Single Linked Lists.py
+++ Single Linked Lists.py
@@ -8,22 +8,39 @@ class LinkedList:
     def __init__(self):
             self.head = None
 
-"""Display each element in the linked list"""
-def display(self):
-    current = self.head
-    while current:
-        print(current.data, end=" -> ") # the format of printing depends on the problem
-        current = current.next
-    print("None")
-"""Search the node at index i"""
-def findAt(self, index):
-    current = self.head
-    if not current:
-        return None
-    while index>0:
-        current = current.next
+    """Display each element in the linked list"""
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ") # the format of printing depends on the problem
+            current = current.next
+        print("None")
+    """Search the node at index i"""
+    def findAt(self, index):
+        current = self.head
         if not current:
             return None
-        index-=1
-    return current
+        while index>0:
+            current = current.next
+            if not current:
+                return None
+            index-=1
+        return current
+
+    """Insert a node in the front"""
+    def insert_at_front(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    """ Insert a node in the back"""
+    def insert_at_back(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+            last_node.next = new_node
 
