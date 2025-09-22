10f39c85060b8148f1db1b0c21df4f92be35253a
BOWEN YIP WEI DE <98448831+bowenyipweide@users.noreply.github.com>
2025-09-22 16:38:34 +0800

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,21 @@
+Concepts and Applications of Data Structures:
+{The data needs to be stored and managed appropriately so that we can retrieve and process the information efficiently}
+• Arrays
+• Linked Lists
+• Stacks
+• Queues
+• Trees
+
+2 computational problems: 
+1. Searching
+- Find and search key in a given set
+2. Sorting
+- Rearrange data of a given set in certain order
+
+Algorithm Design Strategies:
+• Brute Force and Exhaustive Search
+• Divide-and-Conquer
+• Greedy Strategy
+• Decrease-and-Conquer
+• Transform-and-Conquer
+• Iterative Improvement
